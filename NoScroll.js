7351e49c2f74a53226b2c21de5f4605c32c079b7
R00tFSDev <149910442+R00tfsdev@users.noreply.github.com>
2024-03-13 11:16:26 -0400

    Create NoScroll.js

---
+++ NoScroll.js
@@ -0,0 +1,6 @@
+// JavaScript to prevent scrolling
+// Original Github https://github.com/JSScripterDude/NoScroll
+document.addEventListener('DOMContentLoaded', function() {
+    // Disables scrolling might not work for trashy ass browsers like wave or sum shit
+    document.body.style.overflow = 'hidden';
+});
